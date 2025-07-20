277a4e207c7a0401a1faf469a8817704e3969262
Souhardya20 <souhardyadeb2016@gmail.com>
2025-07-21 01:02:34 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
+    domains: ["localhost","kanchenjunga-backend.jgecalumni.in"],
   },
   eslint: {
     ignoreDuringBuilds: true,
